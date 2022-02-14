c13cc74ba49a5ff3736e2b0a84b826d3d4b2218a
MichaelJAliberti <mjali@bu.edu>
2022-02-14 02:02:53 -0500

    added data format checks

--- src/device/device_interface.py
+++ src/device/device_interface.py
@@ -8,7 +8,32 @@ data_template = ["mac", "value"]
 
 
 def check_mac_format(mac):
-    return re.match("([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$", mac)
+    """Take in mac_address, determine if its format is valid
+
+    :param mac: mac address for a device
+    :type mac: string
+    ...
+    :return: true if valid, false if not
+    :rtype: bool
+    """
+    return bool(re.match("([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$", mac))
+
+
+def check_valid_data(device_data):
+    """Take in mac_address, determine if its format is valid
+
+    :param device_data: mac address for a device
+    :type device_data: dictionary
+    ...
+    :return: true if valid, false if not
+    :rtype: bool
+    """
+    if not all([data_type in device_data for data_type in data_template]):
+        logging.warning("Insufficient data fields")
+        return False
+    if not check_mac_format(device_data["mac"]):
+        logging.warning(f"Invalid mac address: {device_data['mac']}")
+        return False
 
 
 def read_from_device(file):
@@ -22,8 +47,9 @@ def read_from_device(file):
     """
     with open(file) as f:
         device_data = json.load(f)
-    if not check_mac_format(device_data["mac"]):
-        logging.warning(f"Invalid mac address: {device_data['mac']}")
+    if not check_valid_data(device_data):
+        return {}
+    trim_data(device_data)
     device_data["timestamp"] = datetime.now(timezone.utc)
 
     return device_data
@@ -39,6 +65,16 @@ def retrieve_db_info(mac_address):
     :rtype: dictionary
     """
     pass
+
+
+def trim_data(device_data):
+    """Removes unnecessary data from device stream
+
+    :param mac_address: identifier for device
+    :type mac_address: string
+    """
+    excess_fields = [key for key in device_data if key not in data_template]
+    [device_data.pop(field) for field in excess_fields]
 
 
 if __name__ == "__main__":
