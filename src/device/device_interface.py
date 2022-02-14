90fa672eee8739026c83944a88d1b44bbde22835
MichaelJAliberti <mjali@bu.edu>
2022-02-13 21:22:23 -0500

    added empty functions

---
+++ src/device/device_interface.py
@@ -0,0 +1,19 @@
+def read_from_device():
+    """ Take in mac_address and readout_value of a given device
+
+    :return: a dictionary of device data, timestamp, and error codes
+    :rtype: dictionary
+    """
+    pass
+
+
+def retrieve_db_info(mac_address):
+    """ Look up device information given its mac_address
+
+    :param mac_address: identifier for device
+    :type mac_address: string
+    ...
+    :return: a dictionary of device information
+    :rtype: dictionary
+    """
+    pass
