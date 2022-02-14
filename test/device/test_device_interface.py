4e3fa90f85d05abc773601efb6e01c877dd5c9ad
MichaelJAliberti <mjali@bu.edu>
2022-02-14 18:40:34 -0500

    added testing stubs

--- test/device/test_device_interface.py
+++ test/device/test_device_interface.py
@@ -1,6 +1,31 @@
 from src.device.device_interface import DeviceInterface
 
 
-def test_placeholder():
+def test_check_data_format():
+    DeviceInterface("data/device.json")
+    assert True
+
+
+def test_check_file():
+    DeviceInterface("data/device.json")
+    assert True
+
+
+def test_check_mac_format():
+    DeviceInterface("data/device.json")
+    assert True
+
+
+def test_read_from_device():
+    DeviceInterface("data/device.json")
+    assert True
+
+
+def test_retrieve_db_data():
+    DeviceInterface("data/device.json")
+    assert True
+
+
+def test_trim_data():
     DeviceInterface("data/device.json")
     assert True
