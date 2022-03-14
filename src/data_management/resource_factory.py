0ec58c13c2e80ed119c494bb8e87029587d2363e
MichaelJAliberti <mjali@bu.edu>
2022-03-14 18:39:48 -0400

    reverted Heroku tests

--- src/data_management/resource_factory.py
+++ src/data_management/resource_factory.py
@@ -13,7 +13,7 @@ def abort_if_operation_unsupported(operation, name):
 class ResourceFactory:
     @classmethod
     def make_resources(cls, *, name, sample, required_fields=[]):
-        data = {"filler": "Hello World"}
+        data = {}
         resources = []
 
         put_parser, post_parser = _get_parsers(
