4817e31ebc4aed8562274a9cb6472945a7108e29
MichaelJAliberti <mjali@bu.edu>
2022-03-14 18:20:48 -0400

    prepping for Heroku deploy

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# from src.utils.file import check_file
-# from src.device.device_interface import DeviceInterface
-
 from src.data_management.restful_service import RESTService
 from src.data_management.resource_factory import ResourceFactory
 
@@ -19,27 +16,6 @@ if __name__ == "__main__":
         "InsuranceGroupID": "U57",
     }
 
-    sample2 = {
-        "FullName": "John Doe",
-        "Email": "example@example.com",
-        "DoB": "1/1/2000",
-        "Gender": "male",
-        "Weight": 80.2,
-        "Height": 156,
-        "PrimaryContact": "Jane Doe",
-        "SecondaryContact": "Jake Doe",
-        "Address": "1 Road Rd",
-        "Insurance": "Blue Cross",
-        "InsuranceGroupID": "U57",
-    }
-
     resources = ResourceFactory.make_resources(name="user", sample=sample)
-    # for element in ResourceFactory.make_resources(name="what", sample=sample):
-    #     resources.append(element)
-    # for element in ResourceFactory.make_resources(name="why", sample=sample):
-    #     resources.append(element)
-    # for element in ResourceFactory.make_resources(name="how", sample=sample):
-    #     resources.append(element)
-
     service = RESTService.build_from_resources(resources)
     service.app.run(debug=True)
