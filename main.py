a2f4f17206385f737144c50d83b77ed323b2f05f
MichaelJAliberti <mjali@bu.edu>
2022-03-17 10:08:32 -0400

    updated api ocmponents in main

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from src.data_management.restful_service import RESTService
 from src.data_management.resource_factory import ResourceFactory
 
-sample = {
-        "FullName": "John Doe",
-        "Email": "example@example.com",
-        "DoB": "1/1/2000",
-        "Gender": "male",
-        "Weight": 80.2,
-        "Height": 156,
-        "PrimaryContact": "Jane Doe",
-        "SecondaryContact": "Jake Doe",
-        "Address": "1 Road Rd",
-        "Insurance": "Blue Cross",
-        "InsuranceGroupID": "U57",
-    }
+user_sample = {
+    "FullName": "John Doe",
+    "Email": "example@example.com",
+    "DoB": "1/1/2000",
+    "Gender": "male",
+    "Weight": 80.2,
+    "Height": 156,
+    "PrimaryContact": "Jane Doe",
+    "SecondaryContact": "Jake Doe",
+    "Address": "1 Road Rd"
+}
 
-resources = ResourceFactory.make_resources(name="user", sample=sample)
+device_sample = {
+   "mac":"ff-ff-ff-ff-ff-ff",
+   "value":145,
+   "excess":120
+}
+
+resources = []
+for resource in ResourceFactory.make_resources(name="user", sample=user_sample):
+    resources.append(resource)
+for resource in ResourceFactory.make_resources(name="device", sample=device_sample):
+    resources.append(resource)
+
 service = RESTService.build_from_resources(resources)
 app = service.app
 
