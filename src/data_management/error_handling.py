f98beac6fdc1843a831972b12db7e8f16e2411a5
MichaelJAliberti <mjali@bu.edu>
2022-04-11 21:13:21 -0400

    added docstrings to abort methods

--- src/data_management/error_handling.py
+++ src/data_management/error_handling.py
@@ -1,10 +1,24 @@
 from flask_restful import abort
 
 
-def abort_if_does_not_exist(id, data):
+def abort_if_does_not_exist(key, data):
+    """throws a curl status error if a given key does not exist in a dictionary
+    
+    :param key: key into data
+    :type: str
+    :param data: dictionary of api data
+    :type: dict
+    """
     if id not in data:
-        abort(404, message=f"Element {id} doesn't exist.")
+        abort(404, message=f"Element {key} doesn't exist.")
 
 
 def abort_if_operation_unsupported(operation, name):
+    """throws a curl status error if a given operation cannot be applied to the calling resource
+    
+    :param operation: curl operation name
+    :type: str
+    :param name: url path to the calling resource
+    :type: str
+    """
     abort(405, message=f"{operation.upper()} not supported for resource {name}.")
